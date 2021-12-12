6165cc67aeece28b5cb3aec6826e910c2e22517c
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-12 18:24:46 +0530

    Create Player.java

---
+++ Files/Player.java
@@ -0,0 +1,16 @@
+class Player {
+    private String name;
+    private int location = 0;
+    Player(String name) {
+        this.name = name;
+    }
+    public void setLocation(int loc) {
+        this.location = loc;
+    }
+    public int getLocation() {
+        return this.location;
+    }
+    public String getName() {
+        return name;
+    }
+}
