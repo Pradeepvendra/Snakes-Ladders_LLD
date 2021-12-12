a7209ef6993aba92bae021314f572d0e705b966f
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-12 18:29:36 +0530

    Create Jumper.java

---
+++ Files/Jumper.java
@@ -0,0 +1,28 @@
+class Jumper {
+    private int startPoint;
+    private int endPoint;
+
+    Jumper(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public int getEndPoint() {
+        return endPoint;
+    }
+}
+
+// Inheritence Snakes and Ladders from jumper
+// NOTE:- for Snakes, startPoint is endPoint and endPoint is startPoint
+
+class Snakes extends Jumper {
+    Snakes(int startPoint, int endPoint) {
+        super(startPoint, endPoint);
+    }
+}
+
+class Ladders extends Jumper {
+    Ladders(int startPoint, int endPoint) {
+        super(startPoint, endPoint);
+    }
+}
