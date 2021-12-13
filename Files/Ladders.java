2f7e4797b293895a2239aafc764b89da0be999d2
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-13 10:11:47 +0530

    Create Ladders.java

---
+++ Files/Ladders.java
@@ -0,0 +1,8 @@
+// Inheritence Ladders from jumper
+// NOTE:- for Snakes, startPoint is endPoint and endPoint is startPoint
+
+public class Ladders extends Jumper {
+    Ladders(int startPoint, int endPoint) {
+        super(startPoint, endPoint);
+    }
+}
