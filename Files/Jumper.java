6c9f0929e4cbf31e151bd99d4bcdb0a735580359
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-13 11:50:10 +0530

    Update Jumper.java

--- Files/Jumper.java
+++ Files/Jumper.java
@@ -11,18 +11,3 @@ public class Jumper {
         return endPoint;
     }
 }
-
-// Inheritence Snakes and Ladders from jumper
-// NOTE:- for Snakes, startPoint is endPoint and endPoint is startPoint
-
-class Snakes extends Jumper {
-    Snakes(int startPoint, int endPoint) {
-        super(startPoint, endPoint);
-    }
-}
-
-class Ladders extends Jumper {
-    Ladders(int startPoint, int endPoint) {
-        super(startPoint, endPoint);
-    }
-}
