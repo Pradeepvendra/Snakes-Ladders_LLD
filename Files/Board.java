978811683386cab7cd4474d5e0ad2748aadd55e9
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-13 11:37:57 +0530

    Update Board.java

--- Files/Board.java
+++ Files/Board.java
@@ -1,11 +1,12 @@
-public class Board {
+class Board {
     private Dice dice;
     private Queue<Player> allPlayer;
     private HashMap<Integer, Snakes> snakes;
     private HashMap<Integer, Ladders> ladders;
     private int boardSize;
 
-    Board(int boardSize, Dice dice, HashMap<Integer, Snakes> snakes, HashMap<Integer, Ladders> ladders,Queue<Player> allPlayer) {
+    Board(int boardSize, Dice dice, HashMap<Integer, Snakes> snakes, HashMap<Integer, Ladders> ladders,
+            Queue<Player> allPlayer) {
         this.boardSize = boardSize;
         this.dice = dice;
         this.snakes = snakes;
@@ -17,27 +18,26 @@ public class Board {
     public void startGame() {
         while (allPlayer.size()>1) {
             Player player = allPlayer.poll();
-            int currLocation = player.getLocation();
-            int nextLocation = currLocation + dice.rollDice();
-            if (nextLocation <= boardSize) {
-                if (snakes.containsKey(nextLocation)) {
-                    System.out.println(player.getName() + " was bitten by a snake! at "+nextLocation);
-                    Snakes obj = snakes.get(nextLocation);
-                    nextLocation = obj.getEndPoint();
-                    } 
-                else if (ladders.containsKey(nextLocation)) {
+            int currLoc = player.getLocation();
+            int nextLoc = currLoc + dice.rollDice();
+            if (nextLoc <= boardSize) {
+                if (snakes.containsKey(nextLoc)) {
+                    System.out.println(player.getName() + " was bitten by a snake! at "+nextLoc);
+                    Snakes snake = snakes.get(nextLoc);
+                    nextLoc = snake.getEndPoint();
+                } else if (ladders.containsKey(nextLoc)) {
                     System.out.println(player.getName() + " climbed a Ladder!");
-                    Ladders obj = ladders.get(nextLocation);
-                    nextLocation = obj.getEndPoint();
-                     }
-                currLocation = nextLocation;
-                System.out.println(player.getName() + " moved to " + currLocation);
-                if (currLocation == boardSize) {
+                    Ladders ladder = ladders.get(nextLoc);
+                    nextLoc = ladder.getEndPoint();
+                }
+                currLoc = nextLoc;
+                System.out.println(player.getName() + " moved to " + currLoc);
+                if (currLoc == boardSize) {
                     System.out.println();
                     System.out.println(player.getName() + " won th Game!!");
                     break;
                 }
-                player.setLocation(currLocation);
+                player.setLocation(currLoc);
 
             }
             allPlayer.add(player);
