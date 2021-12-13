fe29dd3e4e1897eaf08b8fb21864cfd3439fbbf5
Pradeepvendra <61039076+Pradeepvendra@users.noreply.github.com>
2021-12-13 10:12:10 +0530

    Update Snakes.java

--- Files/Snakes.java
+++ Files/Snakes.java
@@ -1,7 +1,7 @@
-// Inheritence Snakes and Ladders from jumper
+// Inheritence Snakes from jumper
 // NOTE:- for Snakes, startPoint is endPoint and endPoint is startPoint...so,startpoint is greater than endpoint.
 
-class Snakes extends Jumper {
+public class Snakes extends Jumper {
     Snakes(int startPoint, int endPoint) {
         super(startPoint, endPoint);
     }
